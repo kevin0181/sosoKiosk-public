dda7eb9fc45c9ded6bd701c5a952c4104e3808e5
youngbeen you <kevin0181@naver.com>
2022-05-04 19:35:31 +0900

    change electron.js

--- kiosk/public/electron.js
+++ kiosk/public/electron.js
@@ -4,14 +4,15 @@ const url = require('url');
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        // width: 800,
+        // height: 600,
         webPreferences: {
             webSecurity: false,
         }
+
     });
     win.kiosk = true;
-    // win.loadFile("./kiosk/index.html");
+    win.setMenu(null);
     const startUrl = process.env.ELECTRON_START_URL || url.format({
         pathname: path.join(__dirname, '/../build/index.html'),
         protocol: 'file:',
