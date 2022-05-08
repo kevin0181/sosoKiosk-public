262bced320c118d29b5216bdbc169b20b44e359b
youngbeen you <kevin0181@naver.com>
2022-05-08 19:12:54 +0900

    salesTop add

--- kiosk/src/pages/admin/saels/SalesTop.js
+++ kiosk/src/pages/admin/saels/SalesTop.js
@@ -1,6 +1,80 @@
-const SalesTop = () => {
+import {useNavigate} from "react-router-dom";
+import $ from "jquery";
+import {useEffect} from "react";
+
+const SalesTop = ({status}) => {
+
+    const navigate = useNavigate();
+
+    const statusCss = () => {
+        switch (status) {
+            case 'close':
+                return $('#kioskClose').addClass('BtnClickColor');
+            case 'sales':
+                return $('#salesTotal').addClass('BtnClickColor');
+            default:
+                return $('#salesTotal').addClass('BtnClickColor');
+        }
+    }
+
+    useEffect(() => {
+        statusCss();
+    });
+
     return (
-        <div>SalesTop</div>
+        <div className="adminTop M-font">
+            <div className="admin-top-top">
+                <div className="admin-top-part admin-top-part-left">
+                    <div className="admin-top-p-part" onClick={() => {
+                        sessionStorage.clear();
+                        navigate('/');
+                    }}>
+                        <p className="" style={{fontSize: '40px'}}>홈으로</p>
+                    </div>
+                </div>
+                <div className="admin-top-part admin-top-part-nomal">
+                    <div className="admin-top-p-part" id='kioskClose' onClick={() => {
+                        navigate('/admin/sales?status=close')
+                    }}>
+                        <p className="" style={{fontSize: '40px'}}>마감 하기</p>
+                    </div>
+                </div>
+                <div className="admin-top-part admin-top-part-nomal">
+                    <div className="admin-top-p-part" id="salesTotal" onClick={() => {
+                        navigate('/admin/sales?status=sales');
+                    }}>
+                        <p className="" style={{fontSize: '40px'}}>매출 계산</p>
+                    </div>
+                </div>
+                <div className="admin-top-part admin-top-part-right">
+                    <div className="admin-top-p-part" onClick="">
+                        <p className="" style={{fontSize: '40px'}}></p>
+                    </div>
+                </div>
+            </div>
+            <div className="admin-top-bottom btnSound">
+                <div className="admin-top-part admin-bottom-part-left">
+                    <div className="admin-top-p-part">
+                        <p className="" style={{fontSize: '40px'}}></p>
+                    </div>
+                </div>
+                <div className="admin-top-part admin-bottom-part-nomal">
+                    <div className="admin-top-p-part">
+                        <p className="" style={{fontSize: '40px'}}></p>
+                    </div>
+                </div>
+                <div className="admin-top-part admin-bottom-part-nomal">
+                    <div className="admin-top-p-part">
+                        <p className="" style={{fontSize: '40px'}}></p>
+                    </div>
+                </div>
+                <div className="admin-top-part admin-bottom-part-right">
+                    <div className="admin-top-p-part">
+                        <p className="" style={{fontSize: '40px'}}></p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
