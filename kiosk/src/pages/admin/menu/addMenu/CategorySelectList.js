025425c0ea38115ac0b3f29e72107001467f0f45
youngbeen you <kevin0181@naver.com>
2022-05-20 14:44:34 +0900

    input css change

--- kiosk/src/pages/admin/menu/addMenu/CategorySelectList.js
+++ kiosk/src/pages/admin/menu/addMenu/CategorySelectList.js
@@ -1,13 +1,11 @@
 const CategorySelectList = ({category}) => {
-
-    console.log(category.length);
     return (
         <>
             <div className="M-input-select-div" id="categorySelectOption">
                 {
                     category.map((it, index) => (
                         <input type="text" defaultValue={it.categoryName} key={it.categorySq}
-                               className="M-input-select M-font M-mini-size M-input-select-middle"/>
+                               className="M-input-select M-menu-input-select M-font M-mini-size M-input-select-middle"/>
                     ))
                 }
             </div>
