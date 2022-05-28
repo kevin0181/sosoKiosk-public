b89a7f5dc907cb251c20c73983bcd36cd41ae410
youngbeen you <kevin0181@naver.com>
2022-05-28 14:47:16 +0900

    카테고리 css 변경 클릭에따라

--- kiosk/src/pages/kiosk/side/CategoryList.js
+++ kiosk/src/pages/kiosk/side/CategoryList.js
@@ -1,23 +1,36 @@
 import {useNavigate} from "react-router-dom";
+import $ from 'jquery';
 
 const CategoryList = ({categoryList}) => {
 
     const navigate = useNavigate();
 
+    const changeSelectCss = (categorySq) => {
+
+        $('.O-category-select').removeClass('O-click-color');
+
+        $('#categorySelectCss' + categorySq).children('.O-category-select').addClass('O-click-color');
+
+    }
+
     return (
         <>
             <div className="text M-font O-category-font O-category-Side" onClick={() => {
-                navigate('/menuOrder')
-            }}>
-                <div className="O-category-name O-click-color">
+                changeSelectCss(0);
+                navigate('/menuOrder?categorySq=0')
+            }} id={'categorySelectCss0'}>
+                <div className="O-category-name O-category-select O-click-color">
                     <a className="O-category-a">전체</a>
                 </div>
             </div>
             {categoryList && categoryList.map((it) => (
-                <div className="text M-font O-category-font O-category-Side" key={it.categorySq} onClick={() => {
-                    navigate('/menuOrder?categorySq=' + it.categorySq)
-                }}>
-                    <div className="O-category-name">
+                <div className="text M-font O-category-font O-category-Side" key={it.categorySq}
+                     id={'categorySelectCss' + it.categorySq}
+                     onClick={() => {
+                         changeSelectCss(it.categorySq);
+                         navigate('/menuOrder?categorySq=' + it.categorySq)
+                     }}>
+                    <div className="O-category-name O-category-select">
                         <p className="O-category-a">{it.categoryName}</p>
                     </div>
                 </div>
