db1e21c1cab0629be80b3770f490e138fae2fcf2
youngbeen you <kevin0181@naver.com>
2022-05-31 21:06:11 +0900

    현금 주문 시 메뉴 DB에 저장 성공

--- kiosk/src/js/kiosk/money.js
+++ kiosk/src/js/kiosk/money.js
@@ -1,23 +1,42 @@
 import {longReceipt} from './receipt';
 import {shortReceipt} from "./receipt";
+import axios from "axios";
+import serverUrl from "../../pages/config/server.json";
 
 export const NoReceiptMoneyPayStart = async (data) => { //영수증 출력 X
-
     saveMoneyOrder(data).then(function () {
         shortReceipt();
-    })
-
+    });
 }
 
 export const YesReceiptMoneyPayStart = async (data) => { //영수증 출력 O
-
     saveMoneyOrder(data).then(function () {
         longReceipt();
-    })
-
+    });
 }
 
 const saveMoneyOrder = async (data) => { //주문 시 저장 로직
-    console.log(data);
 
+    saveData(data).then(function (res) {
+        console.log(res);
+    });
+
+}
+
+const saveData = async (data) => { //주문 DB 전송 저장
+
+    let sendData = {
+        "orderMenu": data.orderMenu,
+        "totalPrice": (data.totalPrice).toString(),
+        "placeStatus": data.placeStatus,
+        "payStatus": data.payStatus
+    }
+
+
+    const response = await axios.post('http://' + serverUrl.server + '/kiosk/menu/order/menu', sendData, {
+        headers: {
+            "Content-Type": `application/json`,
+        },
+    });
+    return response.data;
 }
