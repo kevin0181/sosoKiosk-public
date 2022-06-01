08365351c2423a547f7bde2050a069749e13d080
youngbeen you <kevin0181@naver.com>
2022-06-01 11:13:37 +0900

    키오스크 세팅값 가져옴

--- kiosk/src/js/kiosk/receipt.js
+++ kiosk/src/js/kiosk/receipt.js
@@ -3,13 +3,29 @@ import serverUrl from "../../pages/config/server.json";
 import {Stomp} from "@stomp/stompjs";
 import SockJS from "sockjs-client";
 
+let getSettingTax;
+let leaderName;
+let readerNo;
+let businessNumber;
+
+let issueID = 1;
+let _inch = 2;
+
+let printerName = "Printer1";
+
+
 export const longReceipt = (saveData, orderNumber) => { //영수증 출력 O
     if (saveData === null) {
         alert("주문을 저장할 수 없습니다. 관리자를 호출해주세요 (error : 1001)");
         return false;
     }
 
-    sendByServerOrder(saveData, orderNumber);
+    sendByServerOrder(saveData, orderNumber).then(function () {
+        console.log(123);
+        getSettingData().then(function () {
+
+        });
+    });
 }
 
 export const shortReceipt = (saveData, orderNumber) => { //영수증 출력 X
@@ -18,13 +34,15 @@ export const shortReceipt = (saveData, orderNumber) => { //영수증 출력 X
         return false;
     }
 
-    sendByServerOrder(saveData, orderNumber);
+    sendByServerOrder(saveData, orderNumber).then(function () {
+        getSettingData().then(function () {
+
+        });
+    });
 
 }
 
-const sendByServerOrder = (saveData, orderNumber) => {
-
-    console.log(saveData);
+const sendByServerOrder = async (saveData, orderNumber) => {
 
     let moneyStompClient;
     let sosoServerStatus;
@@ -49,11 +67,29 @@ const sendByServerOrder = (saveData, orderNumber) => {
 
 }
 
-const cancelOrderData = async (saveData) => {
-    const response = await axios.post('http://' + serverUrl.server + '/kiosk/menu/cancel/saveMenu', saveData, {
-        headers: {
-            "Content-Type": `application/json`,
-        },
+const getSettingData = async () => {
+    const response = await axios.get('http://' + serverUrl.server + '/kiosk/get/setting', {
+        params: {
+            "setting": "all"
+        }
     });
-    return response.data;
+    response.data.map((it) => {
+        switch (it.settingName) {
+            case 'tax':
+                getSettingTax = Number(it.settingValue);
+                break;
+            case 'readerNo':
+                readerNo = it.settingValue
+                break;
+            case 'leaderName':
+                leaderName = it.settingValue
+                break;
+            case 'businessNumber':
+                businessNumber = it.settingValue
+                break;
+            case 'printerName':
+                printerName = it.settingValue
+                break;
+        }
+    });
 }
