513a15a2f8c51de01b3a1c20eff25318d0abc171
youngbeen you <kevin0181@naver.com>
2022-06-02 18:55:15 +0900

    메뉴 수정 시 사이드 없앨수 있는 기능 추가

--- kiosk/src/pages/admin/menu/addMenu/SideSelectList.js
+++ kiosk/src/pages/admin/menu/addMenu/SideSelectList.js
@@ -2,6 +2,10 @@ const SideSelectList = ({side, changeSide}) => {
     return (
         <>
             <div className="M-input-select-div" id="sideSelectOption">
+                <input type="text" data-id={0} defaultValue={'없음'}
+                       name={'sideSelect'} onClick={changeSide}
+                       className="M-input-select M-side-input-select M-font M-mini-size M-input-select-middle"
+                       readOnly/>
                 {
                     side.map((it) => (
                         <input type="text" defaultValue={it.sideName} key={it.sideSq}
