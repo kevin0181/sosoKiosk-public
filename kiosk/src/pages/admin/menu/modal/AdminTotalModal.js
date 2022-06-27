4bf35fbf74f8f545421a1ec6672759c172d32a5d
kevin <kevin0181@naver.com>
2022-06-27 20:22:53 +0900

    키오스크 전체 변경 완료, 확인 전

--- kiosk/src/pages/admin/menu/modal/AdminTotalModal.js
+++ kiosk/src/pages/admin/menu/modal/AdminTotalModal.js
@@ -21,18 +21,18 @@ const AdminTotalModal = ({modalStatus, modalContentChange}) => {
                         </div>
                         <div className="O-modal-top">
                             <div className="O-modal-top-title M-font" id="deleteTop">
-                                <p>{modalStatus.modalTitle}</p>
+                                <p className={"M-font-30-size"}>{modalStatus.modalTitle}</p>
                             </div>
                         </div>
                     </div>
-                    <div className="O-modal-side-order" style={{height: '40%', textAlign: 'center'}}
+                    <div className="O-modal-side-order M-flex-center M-flex-row" style={{height: '40%', textAlign: 'center'}}
                          id="adminTotalModalBody">
-                        <p className={'M-font O-font-middle-size'}>{modalStatus.modalContent}</p>
+                        <p className={'M-font M-font-25-size'}>{modalStatus.modalContent}</p>
                     </div>
                     <div className="O-modal-side-footer M-flex-j-center" style={{marginTop: '30px', height: '15%'}}>
-                        <div className="O-receipt-modal-btn" onClick={closeBtn}
+                        <div className="O-receipt-modal-btn M-flex-center M-flex-row" onClick={closeBtn}
                              style={{width: '35%', backgroundColor: '#d56161'}}>
-                            <p className="M-font O-font-middle-size">닫기</p>
+                            <p className="M-font M-font-25-size">닫기</p>
                         </div>
                     </div>
                 </div>
