e3b9f018beffeb2ce73309011b07004145eac9e2
kevin <kevin0181@naver.com>
2022-07-13 21:58:00 +0900

    원격 배포 완성

--- kiosk/public/electron.js
+++ kiosk/public/electron.js
@@ -17,7 +17,7 @@ function createWindow() {
         }
     });
     win.kiosk = true;
-    // win.setMenu(null);
+    win.setMenu(null);
     const startUrl = process.env.ELECTRON_START_URL || url.format({
         pathname: path.join(__dirname, '/../build/index.html'),
         protocol: 'file:',
@@ -33,66 +33,25 @@ let progressBar;
 autoUpdater.on('checking-for-update', () => {
     log.info('업데이트 확인 중...');
 });
-
-/* 업데이트가 가능한지 확인하는 부분이다.
-업데이트가 가능한 경우 팝업이 뜨면서 업데이트를 하겠냐고 묻는다.
-Update를 클릭하면 업데이트 가능한 파일을 다운로드 받는다. */
-autoUpdater.on('update-available', () => {
+autoUpdater.on('update-available', (info) => {
     log.info('업데이트가 가능합니다.');
-    dialog
-        .showMessageBox({
-            type: 'info',
-            title: 'Update available',
-            message:
-                'A new version of Project is available. Do you want to update now?',
-            buttons: ['Update', 'Later'],
-        })
-        .then((result) => {
-            const buttonIndex = result.response;
-
-            if (buttonIndex === 0) autoUpdater.downloadUpdate();
-        });
+    progressBar = new ProgressBar({
+        text: '새로운 파일을 다운받고 있습니다.',
+        detail: '잠시만 기다려주세요....',
+    });
 });
-
 autoUpdater.on('update-not-available', (info) => {
     log.info('현재 최신버전입니다.');
 });
 autoUpdater.on('error', (err) => {
     log.info('에러가 발생하였습니다. 에러내용 : ' + err);
 });
-autoUpdater.once('download-progress', (progressObj) => {
-    progressBar = new ProgressBar({
-        text: 'Downloading...',
-        detail: 'Downloading...',
-    });
-    progressBar
-        .on('completed', function () {
-            console.info(`completed...`);
-            progressBar.detail = 'Task completed. Exiting...';
-        })
-        .on('aborted', function () {
-            console.info(`aborted...`);
-        });
-});
 
-// 업데이트를 다운받고 나면 업데이트 설치 후 재시작을 요청하는 팝업이 뜬다.
 autoUpdater.on('update-downloaded', (info) => {
     log.info('업데이트가 완료되었습니다.');
-    progressBar.setCompleted();
-    dialog
-        .showMessageBox({
-            type: 'info',
-            title: 'Update ready',
-            message: 'Install & restart now?',
-            buttons: ['Restart', 'Later'],
-        })
-        .then((result) => {
-            const buttonIndex = result.response;
-
-            if (buttonIndex === 0) autoUpdater.quitAndInstall(false, true);
-        });
+    progressBar.close();
+    app.quit();
 });
-
 
 app.on('window-all-closed', () => {
     app.quit();
